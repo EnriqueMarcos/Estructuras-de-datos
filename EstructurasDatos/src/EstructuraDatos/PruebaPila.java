f8778114a8cb1ab6254af4312383502985fb3330
Enrique Marcos <enriquem.sanred@educa.jcyl.es>
2024-02-18 11:44:32 +0100

    hjg

---
+++ EstructurasDatos/src/EstructuraDatos/PruebaPila.java
@@ -0,0 +1,13 @@
+package EstructuraDatos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PruebaPila {
+	PabloPila<Integer> pila = new PabloPila<Integer>();
+	
+	
+
+}
